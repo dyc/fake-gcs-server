c183ff1d0919286be13d29cc9e7b7e8fe7f69224
Dennis Cheng <dennis.cheng@discordapp.com>
2022-05-27 16:13:21 +0000

    write obj

--- fakestorage/grpc_server.go
+++ fakestorage/grpc_server.go
@@ -2,14 +2,20 @@ package fakestorage
 
 import (
 	"context"
+	"errors"
 	"fmt"
 	"log"
 	"net"
+	"strconv"
 
 	"github.com/fsouza/fake-gcs-server/internal/backend"
+	"github.com/fsouza/fake-gcs-server/internal/checksum"
 	pb "google.golang.org/genproto/googleapis/storage/v2"
 	"google.golang.org/grpc"
+	timestamppb "google.golang.org/protobuf/types/known/timestamppb"
 )
+
+var errUnsupportedWriteOperation = errors.New("unsupported write operation")
 
 type StorageServer struct {
 	pb.UnimplementedStorageServer
@@ -69,6 +75,82 @@ func (s *StorageServer) ListObjects(_ctx context.Context, req *pb.ListObjectsReq
 	}
 
 	return resp, nil
+}
+
+func (s *StorageServer) WriteObject(writeServer pb.Storage_WriteObjectServer) error {
+	req, err := writeServer.Recv()
+	data := req.GetChecksummedData()
+	if data == nil {
+		return errUnsupportedWriteOperation
+	}
+	spec := req.GetWriteObjectSpec()
+	if spec == nil {
+		return errUnsupportedWriteOperation
+	}
+	md5Hash := checksum.EncodedMd5Hash(data.Content)
+	reqObj := backend.Object{
+		ObjectAttrs: backend.ObjectAttrs{
+			BucketName:      spec.Resource.Bucket,
+			Name:            spec.Resource.Name,
+			ContentType:     spec.Resource.ContentType,
+			ContentEncoding: spec.Resource.ContentEncoding,
+			Md5Hash:         md5Hash,
+			Etag:            fmt.Sprintf("%q", md5Hash),
+			ACL:             getObjectACL(spec.PredefinedAcl),
+		},
+		Content: data.Content,
+	}
+	if data.Crc32C != nil {
+		reqObj.Crc32c = fmt.Sprint(*data.Crc32C)
+	}
+	backendObj, err := s.backend.CreateObject(reqObj)
+	if err != nil {
+		return err
+	}
+
+	created := fromBackendObjects([]backend.Object{backendObj})[0]
+	maybeCrc32c, err := strconv.ParseUint(created.Crc32c, 10, 32)
+	if err != nil {
+		return err
+	}
+	crc32c := uint32(maybeCrc32c)
+	respAcls := make([]*pb.ObjectAccessControl, len(created.ACL))
+	for i, acl := range created.ACL {
+		respAcls[i] = &pb.ObjectAccessControl{
+			Role:   string(acl.Role),
+			Id:     acl.EntityID,
+			Entity: string(acl.Entity),
+			Email:  acl.Email,
+			Domain: acl.Domain,
+		}
+		if acl.ProjectTeam != nil {
+			respAcls[i].ProjectTeam = &pb.ProjectTeam{
+				ProjectNumber: acl.ProjectTeam.ProjectNumber,
+				Team:          acl.ProjectTeam.Team,
+			}
+		}
+	}
+	return writeServer.SendAndClose(&pb.WriteObjectResponse{
+		WriteStatus: &pb.WriteObjectResponse_Resource{
+			Resource: &pb.Object{
+				Name:            created.Name,
+				Bucket:          created.BucketName,
+				Size:            int64(len(created.Content)),
+				Generation:      created.Generation,
+				ContentType:     created.ContentType,
+				ContentEncoding: created.ContentEncoding,
+				CreateTime:      timestamppb.New(created.Created),
+				DeleteTime:      timestamppb.New(created.Deleted),
+				UpdateTime:      timestamppb.New(created.Updated),
+				Metadata:        created.Metadata,
+				Checksums: &pb.ObjectChecksums{
+					Crc32C:  &crc32c,
+					Md5Hash: []byte(created.Md5Hash),
+				},
+				Acl: respAcls,
+			},
+		},
+	})
 }
 
 func NewStorageServer(options Options) (*StorageServer, error) {
